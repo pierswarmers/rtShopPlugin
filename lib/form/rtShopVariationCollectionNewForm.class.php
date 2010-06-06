e37e14244922df7408bcd27859a782e13db55a0d
Piers Warmers <hello@pierswarmers.com>
2010-06-06 23:44:51 +1000

    Changed class names.

---
+++ lib/form/rtShopVariationCollectionNewForm.class.php
@@ -0,0 +1,28 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class rtShopVariationCollectionNewForm extends sfForm
+{
+  public function configure()
+  {
+    if (!$attribute = $this->getOption('attribute'))
+    {
+      throw new InvalidArgumentException('You must provide a attribute object.');
+    }
+
+    for ($i = 0; $i < $this->getOption('size', 5); $i++)
+    {
+      $variation = new rtShopVariation();
+      $variation->rtShopAttribute = $attribute;
+
+      $form = new rtShopVariationForm($variation);
+
+      $this->embedForm($i, $form);
+    }
+
+    $this->mergePostValidator(new rtShopVariationValidatorSchema());
+  }
+}
