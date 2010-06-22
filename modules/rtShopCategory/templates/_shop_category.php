01cd5e542e0dcfce6f087e9441db8cde3522b627
Piers Warmers <hello@pierswarmers.com>
2010-06-22 13:58:16 +1000

    Changes styles for new spec.

--- modules/rtShopCategory/templates/_shop_category.php
+++
@@ -1,5 +0,0 @@
-<h1><?php echo $rt_shop_category->getTitle() ?></h1>
-
-<div class="rt-page-content clearfix">
-<?php echo markdown_to_html($rt_shop_category->getContent(), $rt_shop_category); ?>
-</div>
--- modules/rtShopCategory/templates/showSuccess.php
+++ modules/rtShopCategory/templates/showSuccess.php
@@ -1,15 +1,17 @@
 <?php use_helper('I18N', 'Date', 'rtText', 'rtForm', 'rtDate', 'rtSite'); ?>
 
-<div class="rt-shop-category-show rt-admin-edit-tools-panel">
+<div class="rt-shop-category rt-show rt-primary-container rt-admin-edit-tools-panel">
   <?php echo link_to(__('Edit'), 'rtShopCategoryAdmin/edit?id='.$rt_shop_category->getId(), array('class' => 'rt-admin-edit-tools-trigger')) ?>
   <h1><?php echo $rt_shop_category->getTitle() ?></h1>
-  <div class="rt-page-content rt-panel clearfix">
+  <div class="rt-container">
     <?php echo markdown_to_html($rt_shop_category->getContent(), $rt_shop_category); ?>
   </div>
-  <div class="rt-collection rt-panel clearfix">
-    <?php foreach($pager as $rt_shop_product): ?>
+  <div class="rt-container rt-collection">
+    <?php $i = 1; foreach($pager as $rt_shop_product): ?>
+    <div class="rt-list-item rt-list-item-<?php echo $i ?>">
     <?php include_partial('rtShopProduct/shopProductMini', array('rt_shop_product' => $rt_shop_product)); ?>
-    <?php endforeach; ?>
+    </div>
+    <?php $i++; endforeach; ?>
   </div>
 </div>
 
