0dad1b1afda7b49f712f532c043d0fe9c1b404f4
Torsten Hirsch <torsten@wranglers.com.au>
2010-06-23 17:40:43 +1000

    add class to a-anker and adjust javascript for gallery

--- modules/rtShopProduct/templates/_order_panel.php
+++ modules/rtShopProduct/templates/_order_panel.php
@@ -74,7 +74,7 @@ use_stylesheet('/rtShopPlugin/css/main.css', 'last');
     $(".rt-shop-option-set").find(':radio').click(function() {
       // run image selection switch
       var match = $(this).attr("title").toLowerCase().replace(/ /g, "_").replace(/-/g, "_");
-      $(".rt-shop-product-primary-image a[title*="+match+"]").css("display","inline").siblings('a').css("display","none");
+      $(".rt-shop-product-primary-image a[class*=rt-image-ref-"+match+"]").css("display","inline").siblings('a').css("display","none");
       // de-focus all options
       $(".rt-shop-option-set input[type=radio]").each(function(){
         $(this).button( "widget" ).fadeTo(1, 0.3).removeClass('available');
