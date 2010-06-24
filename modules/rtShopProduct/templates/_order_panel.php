27c1e25efc12e502f6fbe0995ab7a18f1d5b648b
Torsten Hirsch <torsten@wranglers.com.au>
2010-06-24 12:05:28 +1000

    add javascript for enable/disable add to cart button

--- modules/rtShopProduct/templates/_order_panel.php
+++ modules/rtShopProduct/templates/_order_panel.php
@@ -62,11 +62,10 @@ use_stylesheet('/rtShopPlugin/css/main.css', 'last');
   </p>
 
   <?php if(sfConfig::get('rt_shop_ordering_enabled', true)): ?>
-  <p><button type="submit"><?php echo __('Add to Cart') ?></button></p>
+    <p><button type="submit"><?php echo __('Add to Cart') ?></button></p>
   <?php endif; ?>
 
   <script type="text/javascript">
-
   $("form.rt-shop-product-order-panel").hide();
 
   $(function() {
@@ -77,17 +76,64 @@ use_stylesheet('/rtShopPlugin/css/main.css', 'last');
       $(".rt-shop-product-primary-image a[class*=rt-image-ref-"+match+"]").css("display","inline").siblings('a').css("display","none");
       // de-focus all options
       $(".rt-shop-option-set input[type=radio]").each(function(){
+        
         $(this).button( "widget" ).fadeTo(1, 0.3).removeClass('available');
       });
       // focus available options based on stock id matrix
-      $($(this).next('.ref').html()).each(function(){ $(this).button( "widget" ).fadeTo(1, 1).addClass('available') });
+      $($(this).next('.ref').html()).each(function(){
+        
+        $(this).button( "widget" ).fadeTo(1, 1).addClass('available')
+      });
+      checkUserSelection();
     }).each(function(){
       if($(this).button( "widget" ).hasClass('unavailable')) {
         $(this).button('disable', true);
       }
     });
     $("form.rt-shop-product-order-panel").show();
+    disableSubmitButton("form.rt-shop-product-order-panel button");
   });
+
+  disableSubmitButton = function(id) {
+    $(id).text("Selection not available");
+    $(id).attr("disabled","disabled");
+    $(id).css("color","#CCC");
+    $(id).css("cursor","default");
+  }
+  enableSubmitButton = function(id) {
+    $(id).text("Add to Cart");
+    $(id).attr("disabled",false);
+    $(id).css("color","#000");
+    $(id).css("cursor","pointer");
+  }
+  checkUserSelection = function() {
+    var count_available_items   = 0;
+    var count_selection_groups  = 0;
+    // count the ammount of selection groups
+    $("form.rt-shop-product-order-panel p.selectionGroup").each(function()
+    {
+      count_selection_groups++;
+    })
+    // each input with an stock class
+    $("form.rt-shop-product-order-panel input[class*=rt-shop-stock-id]").each(function()
+    {
+      // get the stock ID of the input field
+      var id = $(this).attr('id');
+      // each label for a stock input field, which has the classes "available" as well as "ui-state-active"
+      $("form.rt-shop-product-order-panel label[for="+id+"][class*=available][class*=ui-state-active]").each(function()
+      {
+        // count those labels
+        count_available_items++;
+        // if the amount of these labels is the same with the selection groups, we have an available combination
+        if(count_available_items == count_selection_groups)
+        {
+          enableSubmitButton("form.rt-shop-product-order-panel button");
+        } else {
+          disableSubmitButton("form.rt-shop-product-order-panel button");
+        }
+      })
+    })
+  }
   </script>
 </form>
 <?php else: ?>
