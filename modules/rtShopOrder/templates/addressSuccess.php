df1b7686cef2770a4f9cfd16f4f2bf511a7eb09d
SeaShadow <konny@wranglers.com.au>
2010-06-24 15:40:58 +1000

    Changed template structure for address and payment template

--- modules/rtShopOrder/templates/addressSuccess.php
+++ modules/rtShopOrder/templates/addressSuccess.php
@@ -1,29 +1,30 @@
 <?php use_helper('I18N', 'Date', 'Number', 'rtText', 'rtForm', 'rtDate', 'rtSite') ?>
 
-<?php $heading_tag = sfConfig::has('app_rt_shop_address_title') ? 'h2' : 'h1' ?>
-<div class="rt-shop-order-address">
+<div class="rt-shop-order rt-address rt-primary-container">
   <h1><?php echo __(sfConfig::get('app_rt_shop_address_title', 'Address')) ?></h1>
 
-  <?php include_partial('breadcrumb', array('sf_request' => $sf_request)) ?>
-  <?php include_partial('rtShopOrder/flashes') ?>
+  <form action="<?php echo url_for('@rt_shop_order_address') ?>" method="post">
+    <div class="rt-container">
+      <?php include_partial('breadcrumb', array()) ?>
 
-  <?php echo form_tag(url_for('@rt_shop_order_address'), array('id' => 'order_address', 'name' => 'order_address')); ?>
-    <h2><?php echo __('Email address for order') ?></h2>
-    <?php include_partial('form', array('form' => $order_form)) ?>
+      <h2><?php echo __('Email address for order') ?></h2>
+      <?php include_partial('form', array('form' => $order_form)) ?>
 
-    <h2><?php echo __('Shipping address') ?></h2>
-    <?php include_partial('form', array('form' => $shipping_order_form)) ?>
+      <h2><?php echo __('Shipping address') ?></h2>
+      <?php include_partial('form', array('form' => $shipping_order_form)) ?>
 
-    <h2><?php echo __('Billing address') ?></h2>
-    <p>
-      <label for="billing_address_shown"><?php echo __('Same As Shipping Address:') ?> <input id="billing_address_shown" type="checkbox" name="billing_address_shown" <?php echo ($billing_address_shown) ? 'checked' : '' ?> /></label>
-    </p>
+      <h2><?php echo __('Billing address') ?></h2>
+      <p>
+        <label for="billing_address_shown"><?php echo __('Same As Shipping Address:') ?> <input id="billing_address_shown" type="checkbox" name="billing_address_shown" <?php echo ($billing_address_shown) ? 'checked' : '' ?> /></label>
+      </p>
 
-    <div id="steer_shop_billing_address" style="<?php echo ($billing_address_shown) ? 'display: none' : 'display: block'; ?>">
-      <?php include_partial('form', array('form' => $billing_order_form)) ?>
+      <div id="steer_shop_billing_address" style="<?php echo ($billing_address_shown) ? 'display: none' : 'display: block'; ?>">
+        <?php include_partial('form', array('form' => $billing_order_form)) ?>
+      </div>
+
     </div>
 
-    <div class="rt-shop-address-actions">
+    <div class="rt-container rt-shop-order-tools">
       <button type="submit" class="steer_shop_address_actions_submit button"><?php echo __('Proceed to payment') ?></button>
     </div>
   </form>
