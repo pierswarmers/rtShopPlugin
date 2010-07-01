1901cf7ea4e72826d69692b8663a5f8b208ea091
SeaShadow <konny@wranglers.com.au>
2010-07-01 15:08:58 +1000

    Template changes for address and checkout

--- modules/rtShopOrder/templates/addressSuccess.php
+++ modules/rtShopOrder/templates/addressSuccess.php
@@ -1,4 +1,4 @@
-<?php use_helper('I18N', 'Date', 'Number', 'rtText', 'rtForm', 'rtDate', 'rtSite') ?>
+<?php use_helper('I18N', 'Number', 'rtForm') ?>
 
 <div class="rt-shop-order rt-address rt-primary-container">
   <h1><?php echo __(sfConfig::get('app_rt_shop_address_title', 'Address')) ?></h1>
@@ -7,14 +7,17 @@
     <div class="rt-container">
       <?php include_partial('breadcrumb', array()) ?>
 
-      <h2><?php echo __('Email address for order') ?></h2>
-      <?php  echo $form['email']->renderRow() ?>
+      <h2><?php echo __('Your Email Address') ?></h2>
+      <table class="rt-admin-toggle-panel-content">
+        <tbody>
+          <?php  echo render_form_row($form['email']) ?>
+        </tbody>
+      </table>
+
       <?php  echo $form->renderHiddenFields() ?>
 
-
-      <h2><?php echo __('Billing address') ?></h2>
-
-        <?php include_partial('address_form', array('form' => $form['billing_address'])) ?>
+      <h2><?php echo __('Billing Address') ?></h2>
+      <?php include_partial('address_form', array('form' => $form['billing_address'])) ?>
  
       <p><label for="shipping_toggle"><?php echo __('Same As Shipping Address:') ?> <input id="shipping_toggle" type="checkbox" name="shipping_toggle" <?php echo ($show_shipping) ? '' : 'checked' ?> /></label></p>
 
