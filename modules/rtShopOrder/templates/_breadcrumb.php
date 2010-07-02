06c241f8a6ed939e31916f916967013d9ce78c83
Piers Warmers <hello@pierswarmers.com>
2010-07-02 14:22:15 +1000

    Simplyfied the display logic.

--- modules/rtShopOrder/templates/_breadcrumb.php
+++ modules/rtShopOrder/templates/_breadcrumb.php
@@ -1,59 +1,18 @@
-<?php $action = $sf_request->getParameter('action'); ?>
+<?php
+$action = $sf_request->getParameter('action');
+
+$links = array(
+    'cart'     => array('position' => 1,       'title' => 'Cart',    'route' => 'rt_shop_order_cart'),
+    'checkout' => array('position' => 2,       'title' => 'Checkout','route' => 'rt_shop_order_checkout'),
+    'address'  => array('position' => 3,       'title' => 'Address', 'route' => 'rt_shop_order_address'),
+    'payment'  => array('position' => 4,       'title' => 'Payment', 'route' => 'rt_shop_order_payment'),
+    'reciept'  => array('position' => 5,       'title' => 'Cart',    'route' => 'rt_shop_order_cart')
+);
+?>
 <ul class="rt-container rt-shop-order-breadcrumb">
-  <li class="<?php
-    if($action === 'cart') {
-      echo 'here';
-    } else if ($action === 'checkout') {
-      echo 'past-present';
-    } else {
-      echo 'past';
-    }
-  ?>">
-    <?php echo link_to_if($action !== 'cart', 'Cart','@rt_shop_order_cart') ?>
-  </li>
-  <li class="<?php
-    switch ($action) {
-      case 'checkout':
-        echo "here";
-        break;
-      case 'address':
-        echo "past-present";
-        break;
-      case 'cart':
-        echo "";
-        break;
-      default:
-        echo "past";
-        break;
-    }
-  ?>">
-    <?php echo link_to_if($action !== 'checkout', 'Checkout','@rt_shop_order_checkout') ?>
-  </li>
-  <li class="<?php
-    switch ($action) {
-      case 'address':
-        echo "here";
-        break;
-      case 'payment':
-        echo "past-present";
-        break;
-      default:
-        echo "";
-        break;
-    }
-  ?>">
-    <?php echo link_to_if($action !== 'address', 'Address','@rt_shop_order_address') ?>
-  </li>
-  <li class="<?php
-    switch ($action) {
-      case 'payment':
-        echo "present";
-        break;
-      default:
-        echo "future";
-        break;
-    }
-  ?>">
-    <?php echo link_to_if($action !== 'payment', 'Payment','@rt_shop_order_payment') ?>
-  </li>
+<?php $class = ' class="past"'; foreach($links as $key => $link): ?>
+  <?php $class = ($key === $action) ? ' class="here"' : $class; ?>
+  <li<?php echo $class ?>><?php echo link_to_if($action !== $key, $link['title'],$link['route']) ?></li>
+  <?php $class = ($class == ' class="here"') ? '' : $class; ?>
+<?php endforeach; ?>
 </ul>
