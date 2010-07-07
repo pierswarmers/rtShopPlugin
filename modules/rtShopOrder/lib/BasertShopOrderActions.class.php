6386c0fcadff469dfcef65bbd9eca0a3b890c714
SeaShadow <konny@wranglers.com.au>
2010-07-07 16:41:59 +1000

    Reenable session cleaning

--- modules/rtShopOrder/lib/BasertShopOrderActions.class.php
+++ modules/rtShopOrder/lib/BasertShopOrderActions.class.php
@@ -492,7 +492,7 @@ class BasertShopOrderActions extends sfActions
       $this->logMessage('{rtShopReceipt} Order #'.$cm->getOrder()->getReference().' was successful but confirmation email could not be sent due to missing admin email in configuration.');
     }
 
-    //$this->cleanSession();
+    $this->cleanSession();
   }
 
   /**
