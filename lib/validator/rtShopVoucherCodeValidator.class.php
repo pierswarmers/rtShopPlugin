0c7aa1798975df291c6faea3556fdf4f7a658def
SeaShadow <konny@wranglers.com.au>
2010-09-09 15:37:41 +1000

    Added new voucher code validator. Checks for length, uniqueness and also cleans code of special characters

---
+++ lib/validator/rtShopVoucherCodeValidator.class.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of the rtShopPlugin package.
+ * (c) 2006-2008 digital Wranglers <steercms@wranglers.com.au>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * rtShopVoucherCodeValidator
+ *
+ * @package    rtShopPlugin
+ * @subpackage validators
+ * @author     Piers Warmers <piers@wranglers.com.au>
+ * @author     Konny Zurcher <konny@wranglers.com.au>
+ */
+class rtShopVoucherCodeValidator extends sfValidatorBase
+{
+  protected function configure($options = array(), $messages = array())
+  {
+    parent::configure($options, $messages);
+  }
+
+  protected function doClean($value)
+  {
+    if(!is_null($value) && $value !== '')
+    {
+      // Replace space with hypen
+      $clean = ereg_replace("[[:space:]]", "-", $value);
+      // Removed special characters: Allowed are a-z, A-Z, 0-9,_ (under-dash),- (hypen)
+      $clean= preg_replace('/[^\w\d_-]/si', '', $clean);
+      // Uppercase string
+      $clean = strtoupper($clean);
+    }
+    else
+    {
+      throw new sfValidatorError($this, 'code', array('code' => $values['code']));
+    }
+
+    return $clean;
+  }
+}
