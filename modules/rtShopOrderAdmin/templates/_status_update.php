cb61dea73e236b23d67695da4d095275305bf76b
Konny <konny@wranglers.com.au>
2010-09-10 16:12:13 +1000

    Added new partial for status update

---
+++ modules/rtShopOrderAdmin/templates/_status_update.php
@@ -0,0 +1,16 @@
+<?php $status = sfConfig::get('app_rt_shop_order_status_types',array('pending', 'paid', 'picking', 'sending', 'sent')); ?>
+<form id ="rtShopOrderStatusUpdate" action="/rtShopOrderAdmin/statusUpdate-?id=<?php echo $rt_shop_order->getId() ?>" method="post">
+  <p><strong><?php echo __('Adjust order status') ?>:</strong><br/>
+    <select name="rt-shop-order-status">
+    <?php foreach($status as $key => $value): ?>
+      <option value="<?php echo $value ?>" <?php echo ($rt_shop_order->getStatus() == $value) ? "selected" : '' ?>><?php echo ucfirst($value) ?></option>
+    <?php endforeach; ?>
+  </select> <button class="status"><?php echo __('Update Status') ?></button></p>
+</form>
+<script type="text/javascript">
+	$(function() {
+    $("#rtShopOrderStatusUpdate .status").button({
+      icons: { primary: 'ui-icon-refresh' }
+    }).submit();
+	});
+</script>
