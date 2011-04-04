247bfb5a067185f645f5eb9d5a35cb72d362a7c3
Konny <konny@wranglers.com.au>
2011-04-04 15:07:05 +1000

    Initial commit

---
+++ lib/interface/rtShopCartManager.interface.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the rtShopPlugin package.
+ *
+ * (c) 2008-2010 digital Wranglers <steercms@wranglers.com.au>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * rtShopCartManagerInterface
+ *
+ * @package    rtShopPlugin
+ * @subpackage interface
+ * @author     Piers Warmers <piers@wranglers.com.au>
+ * @author     Konny Zurcher <konny@wranglers.com.au>
+ */
+interface rtShopCartManagerInterface
+{
+}
