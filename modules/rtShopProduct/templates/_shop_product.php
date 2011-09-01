d9e91101dfbce60f7b5aed0ebbdb884c4d6e70d4
Piers Warmers <hello@pierswarmers.com>
2011-09-01 11:53:03 +1000

    Adjusted order of product widgets.

--- modules/rtShopProduct/templates/_shop_product.php
+++ modules/rtShopProduct/templates/_shop_product.php
@@ -2,7 +2,7 @@
 
 /** @var rtShopProduct $rt_shop_product */
 
-use_helper('Number', 'Url', 'I18N', 'rtShopProduct', 'rtTemplate')
+use_helper('Number', 'Url', 'I18N', 'rtShopProduct', 'rtTemplate', 'rtText')
 
 ?>
 
@@ -34,15 +34,39 @@ use_helper('Number', 'Url', 'I18N', 'rtShopProduct', 'rtTemplate')
 
     <ul class="tabs-content">
       <li class="active" id="orderTab">
-        
-          <?php include_partial('details_preffix', array('rt_shop_product' => $rt_shop_product)) ?>
 
-          <?php include_partial('rtShopProduct/order_panel', array('rt_shop_product' => $rt_shop_product)) ?>
+          <h2>
+            Price: <?php echo price_for($rt_shop_product) ?>
+            <?php if($rt_shop_product->getSku()): ?>
+            <span class="sku"><code>#<?php echo $rt_shop_product->getSku() ?></code></span>
+            <?php endif; ?>
+          </h2>
 
-          <p class="rt-shop-product-tools">
-            <span class="rt-shop-add-to-wishlist"><a href="#"><?php echo __('Add to wishlist') ?></a></span> |
-            <span class="rt-shop-send-to-friend"><a href="<?php echo url_for('rt_shop_send_to_friend', array('product_id' => $rt_shop_product->getId())) ?>"><?php echo __('Send to a friend') ?></a></span>
-          </p>
+          <div class="rt-metas">
+            <?php if($rt_shop_product->getNumberOfComments() > 0): ?>
+            Average Rating: <?php if($rt_shop_product->getOverallRating() > 0) { include_partial('rtComment/rating', array('rating_value' => $rt_shop_product->getOverallRating(), 'show_items' => array('text','graph'))); } ?>
+            <?php endif; ?>
+            <span class="tabs"><a id="commentsTrigger"><?php echo __($rt_shop_product->getNumberOfComments() > 0 ? 'See reviews' : 'Be the first to review') ?></a></span>
+          </div>
+
+
+        <?php include_partial('details_preffix', array('rt_shop_product' => $rt_shop_product)) ?>
+
+        <?php include_partial('rtShopProduct/order_panel', array('rt_shop_product' => $rt_shop_product)) ?>
+
+        <p class="rt-shop-product-tools">
+          <span class="rt-shop-add-to-wishlist"><a href="#"><?php echo __('Add to wishlist') ?></a></span> |
+          <span class="rt-shop-send-to-friend"><a href="<?php echo url_for('rt_shop_send_to_friend', array('product_id' => $rt_shop_product->getId())) ?>"><?php echo __('Send to a friend') ?></a></span>
+        </p>
+
+
+          <div class="description"><?php echo markdown_to_html($rt_shop_product->getContent(), $rt_shop_product); ?></div>
+
+          <script type="text/javascript">
+            $(function() {
+              $('#commentsTrigger').click(function(){ $('a[href="#comments"]').trigger('click'); });
+            });
+          </script>
 
           <?php include_partial('details_suffix', array('rt_shop_product' => $rt_shop_product)) ?>
 
