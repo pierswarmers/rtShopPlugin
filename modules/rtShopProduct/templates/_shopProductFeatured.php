4e987df9f3f0dd45f642579f15c25510d3576fbe
Piers Warmers <hello@pierswarmers.com>
2011-09-14 15:48:47 +1000

    Added holder for featured products.

--- modules/rtShopProduct/templates/_shopProductFeatured.php
+++ modules/rtShopProduct/templates/_shopProductFeatured.php
@@ -1,6 +1,6 @@
 <?php use_helper('I18N', 'Text', 'rtShopProduct') ?>
 
-<div class="rt-shop-product-featured rt-admin-edit-tools-panel rt-admin-edit-tools-panel-small rt-admin-edit-tools-panel-left">
+<div class="rt-shop-product-featured rt-container rt-collection clearfix">
   <?php foreach($rt_shop_products as $rt_shop_product): ?>
     <?php include_partial('rtShopProduct/shopProductMini', array('rt_shop_product' => $rt_shop_product)); ?>
   <?php  endforeach; ?>
