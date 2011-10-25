1181dca898dbb71a17cd044a85f5dd06bdc3c8d9
Piers Warmers <hello@pierswarmers.com>
2011-10-25 15:37:02 +1100

    Added a configurable image dimensions check.

--- modules/rtShopProduct/templates/_shopProductMini.php
+++ modules/rtShopProduct/templates/_shopProductMini.php
@@ -8,6 +8,11 @@ $title = truncate_text (
         true
         );
 
+$config = sfConfig::get('app_rt_gallery');
+
+$img_s_width  = isset($config['product_mini']['max_width'])  ? $config['product_mini']['max_width'] : 350;
+$img_s_height = isset($config['product_mini']['max_height']) ? $config['product_mini']['max_height'] : 450;
+
 $title = str_replace('"', '&quot;', $title);
 
 $mode = isset($mode) ? $mode : '';
@@ -22,7 +27,7 @@ $mode = isset($mode) ? $mode : '';
   <?php echo $mode === 'wishlist' ? link_to(__('Remove from wishlist'), 'rt_shop_show_wishlist', array('delete' => $rt_shop_product->getId()), array('class' => 'delete')) : '' ?>
   <div class="image">
     <?php $promo_span = $rt_shop_product->isOnPromotion() ? '<span class="promotion">'.__('On Sale Now').'</span>' : ''; ?>
-    <?php echo link_to($promo_span.image_tag(rtAssetToolkit::getThumbnailPath($rt_shop_product->getPrimaryImage() ? $rt_shop_product->getPrimaryImage()->getSystemPath() : '', array('maxHeight' => 450, 'maxWidth' => 350)), array('class' => 'primary-image')), 'rt_shop_product_show', $rt_shop_product) ?>
+    <?php echo link_to($promo_span.image_tag(rtAssetToolkit::getThumbnailPath($rt_shop_product->getPrimaryImage() ? $rt_shop_product->getPrimaryImage()->getSystemPath() : '', array('maxHeight' => $img_s_height, 'maxWidth' => $img_s_width)), array('class' => 'primary-image')), 'rt_shop_product_show', $rt_shop_product) ?>
   </div>
   <div class="details">
     <h3><?php echo link_to($title, 'rt_shop_product_show', $rt_shop_product) ?></h3>
